d397b88cc15991e6ca46c86c4406cb1e126631af
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-03-25 04:36:43 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        TwitchChat.GetChat();
+
+        // exit key Ctrl + C
+        var exitSignal = new AutoResetEvent(false);
+        Console.CancelKeyPress += (sender, e) =>
+        {
+            exitSignal.Set();
+
+            e.Cancel = true;
+        };
+
+        exitSignal.WaitOne();
+    }
+
+    //private static void Console_CancelKeyPress(object? sender, ConsoleCancelEventArgs e)
+    //{
+    //    throw new NotImplementedException();
+    //}
+}
---
+++ TwitchChat.cs
@@ -0,0 +1,39 @@
+﻿using StreamFriend;
+using System;
+using TwitchLib.Api;
+using TwitchLib.Api.Helix.Models.ChannelPoints.GetCustomReward;
+using TwitchLib.Api.Helix.Models.Users;
+using TwitchLib.Api.Services;
+using TwitchLib.Client;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Models;
+using TwitchLib.PubSub.Models.Responses.Messages.AutomodCaughtMessage;
+
+class TwitchChat
+{
+    public static void GetChat()
+    {
+        var api = new TwitchAPI();
+        var client = new TwitchClient();
+
+        api.Settings.ClientId = Auth.TwitchClientId;
+        api.Settings.AccessToken = Auth.TwitchAccessToken;
+
+        client.Initialize(new ConnectionCredentials(Auth.TwitchAppName, Auth.OAuthToken));
+
+        client.OnConnected += (sender, e) =>
+        {
+            client.JoinChannel("Damalia_VT");
+        };
+
+        client.Connect();
+
+        client.OnMessageReceived += (sender, e) =>
+        {
+            Console.WriteLine($"[{e.ChatMessage.Username}] {e.ChatMessage.Message}");
+        };
+
+        
+    }
+
+}
